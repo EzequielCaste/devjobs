8fac4b33531fff81b4ccd7bedd2f3d92618e028b
EzequielCaste <ezequiel.caste@gmail.com>
2021-02-12 09:10:18 -0300

    feat: add the JobListContainer and JobListItem
    Uses getServerSideProps on index.js to fetch data from API, sends the data as props to JobListContainer.
    modified:   pages/index.js
    new file:   components/JobListContainer.js
    new file:   components/JobListItem.js
    new file:   styles/components/JobListContainer.scss

---
+++ components/JobListContainer.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+import { JobListItem } from './JobListItem';
+
+export const JobListContainer = ({jobs}) => {
+
+  const [currentJob, setCurrentJob] = useState('');
+
+  const handleClick = (id) => {
+    setCurrentJob(jobs.filter( job => job.id === id));       
+  }
+  const jobListComponents = jobs.map( job => (<div key={job.id} onClick={() => handleClick(job.id)}>
+    <JobListItem job={job} /></div>));    
+
+  return (
+    <div className="JobListContainer">
+      <ul>
+        {jobListComponents}
+      </ul>
+      <section className="showcase">
+        // show job details here
+      </section>
+    </div>
+  )
+}

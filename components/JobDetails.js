4368447900b796888f7fc377a985d1e35f47f5ea
EzequielCaste <ezequiel.caste@gmail.com>
2021-02-17 11:34:11 -0300

    feat: show job details and how to apply link
    Testing modular scss
    modified:   components/JobDetails.js
    modified:   components/JobSearchInput.js
    modified:   components/Navbar.js
    new file:   styles/components/JobDetails.module.scss
    new file:   styles/components/JobDetails.module.scss
    renamed:    styles/components/JobSearchInput.scss -> styles/components/JobSearchInput.module.scss

--- components/JobDetails.js
+++ components/JobDetails.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import styles from '../styles/components/JobDetails.module.scss';
 
-export const JobDetails = ({job}) => {
- const {title, description} = job[0]
-  
+export const JobDetails = ({ job }) => {
+  const {
+    title,
+    description,
+    type,
+    created_at,
+    location,
+    how_to_apply,
+  } = job[0];
+
   return (
-    <div className="JobDetails">
-      <h2>
-      {title}
-      </h2>
-      <p dangerouslySetInnerHTML={{ __html: description}}></p>      
+    <div className={styles.JobDetails}>
+      <h2>{title}</h2>
+      <p className={styles.subtitle}>
+        {type} / {location}
+      </p>
+      <div className={styles.howToApply}>
+        <h5>How to apply</h5>
+        <p dangerouslySetInnerHTML={{ __html: how_to_apply }}></p>
+      </div>
+
+      <div dangerouslySetInnerHTML={{ __html: description }}></div>
     </div>
-  )
-}
+  );
+};

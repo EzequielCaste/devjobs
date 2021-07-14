3df2a3e200b29838114bd579f92e3ecc80eb3f97
EzequielCaste <ezequiel.caste@gmail.com>
2021-07-14 09:31:26 -0300

    remove deprecated GitHub api
    now uses remotive.io

--- components/JobDetails.js
+++ components/JobDetails.js
@@ -1,32 +1,25 @@
 import React from 'react';
 import styles from '../styles/components/JobDetails.module.scss';
 
-export const JobDetails = ({ job }) => {
-  const {
-    title,
-    description,
-    type,
-    created_at,
-    location,
-    how_to_apply,
-  } = job[0];
+export const JobDetails = ({job}) => {
+  const {title, description, job_type, candidate_required_location, url} =
+    job[0];
 
   return (
     <div className={styles['job-details']}>
       <h2>{title}</h2>
       <p className={styles['job-details__subtitle']}>
-        {type} / {location}
+        {job_type} / {candidate_required_location}
       </p>
+      <div dangerouslySetInnerHTML={{__html: description}}></div>
       <div
         className={
           (styles['job-details__how-to-apply'], styles['information-box'])
         }
       >
         <h5 className={styles['box-title']}>How to apply</h5>
-        <p dangerouslySetInnerHTML={{ __html: how_to_apply }}></p>
+        <a href={url}>{url}</a>
       </div>
-
-      <div dangerouslySetInnerHTML={{ __html: description }}></div>
     </div>
   );
 };
--- components/JobListItem.js
+++ components/JobListItem.js
@@ -1,15 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 export const JobListItem = ({job}) => {
-  const {title, company, company_logo,description, type} = job;
+  const {title, company_name, company_logo_url, description, type} = job;
   return (
     <li>
-      <img src={company_logo} alt=""/>
+      <img src={company_logo_url} alt={`Logo for ${company_name} `} />
       <div>
         <span className="job-title">{title}</span>
-        <span className="company-name">{company}</span>
+        <span className="company-name">{company_name}</span>
       </div>
-      
     </li>
-  )
-}
+  );
+};

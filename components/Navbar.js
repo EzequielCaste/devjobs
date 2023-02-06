95a0faebe3a52a7ae732716538ac940f2fe9dfbf
Ezequiel Castellanos <51804994+EzequielCaste@users.noreply.github.com>
2023-02-06 10:08:23 -0300

    merge

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,8 +3,8 @@ import styles from '../styles/components/Navbar.module.scss';
 
 export const Navbar = () => {
   return (
-    <section className={styles.navbar}>
+    <header className={styles.navbar}>
       <h1>DevJobs</h1>
-    </section>
+    </header>
   );
 };
